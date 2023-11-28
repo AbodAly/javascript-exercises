747b4b9427d4f5132bfe503b0032618677f03e19
AbodAly <abdullah2018170546@cis.asu.edu.eg>
2023-11-28 03:23:35 +0100

    Green all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function (firstNumber, secondNumber) {
+  if (
+    (firstNumber < 0) |
+    (secondNumber < 0) |
+    (typeof firstNumber !== "number") |
+    (typeof secondNumber !== "number")
+  )
+    return "ERROR";
+  let start = Math.min(firstNumber, secondNumber);
+  let end = Math.max(firstNumber, secondNumber);
+  let sum = 0;
+  for (start; start <= end; start++) {
+    sum += start;
+  }
+  return sum;
 };
 
 // Do not edit below this line
